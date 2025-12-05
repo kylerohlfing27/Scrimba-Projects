f19d41d48c35a23f48ecadf51ba092132bb8c8d0
Kyle Rohlfing <kylerohlfing27@gmail.com>
2025-12-05 16:57:51 -0700

    Header fleshed out

--- Movie Watchlist/index.js
+++ Movie Watchlist/index.js
@@ -1,1 +1,9 @@
-// index.js
+// Kyle Rohlfing
+// 12/2025
+// Movie Watchlist Scripts - index
+
+const myWatchlistBtn = document.getElementById('my-watchlist-btn')
+
+myWatchlistBtn.addEventListener('click', function() {
+    window.location.href = 'watchlist.html'
+})
--- Movie Watchlist/watchlist.js
+++ Movie Watchlist/watchlist.js
@@ -1,1 +1,9 @@
-// watchlist.js
+// Kyle Rohlfing
+// 12/2025
+// Movie Watchlist Scripts - Watchlist page
+
+const searchMoviesBtn = document.getElementById('search-movies-btn')
+
+searchMoviesBtn.addEventListener('click', function() {
+    window.location.href = 'index.html'
+})
