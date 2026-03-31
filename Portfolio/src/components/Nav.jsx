226b9954ed15175565e946071db2a40f68c38d33
Kyle Rohlfing <kylerohlfing27@gmail.com>
2026-03-31 14:31:20 -0600

    Nav component fleshed out

---
+++ Portfolio/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import logo from "../assets/nav/kr-logo.png"
+
+export default function Nav() {
+    return (
+        <>
+            <div className="nav-container">
+                <img src={logo} alt="logo" className="nav-logo" onClick={logoClick}/>
+                <nav className="nav">
+                    <a href="#about" className="nav-links">About</a>
+                    <a href="#skills" className="nav-links">Skills</a>
+                    <a href="#experience" className="nav-links">Experience</a>
+                    <a href="#projects" className="nav-links">Projects</a>
+                    <a href="#contact" className="nav-links">Contact</a>
+                </nav>
+            </div>
+        </>
+    )
+}
+
+function logoClick() {
+    window.location.href = "#"
+}
